800e2677c1c0c1ae9e646271e6350a212947f949
Pontus Ovhagen <ovhagen@gmail.com>
2017-02-09 09:24:48 +0100

    LED and Switches assignment

---
+++ Lab1/root/address_mapping.h
@@ -0,0 +1,10 @@
+
+#ifndef ADDRESS_MAPPING_H_
+#define ADDRESS_MAPPING_H_
+
+#define SWITCHES_DATA (unsigned int *) 0x40020000
+#define SWITCHES_CONTROL (unsigned int *) 0x40020004
+#define LED_DATA (unsigned int *) 0x40010000
+#define LED_CONTROL (unsigned int *) 0x40010004
+
+#endif /* ADDRESS_MAPPING_H_ */
--- Lab1/root/main.c
+++ Lab1/root/main.c
@@ -1,1 +1,9 @@
-
+#include "address_mapping.h"
+unsigned int state;
+int main() {
+	*SWITCHES_CONTROL = 0xFFFF;
+	*LED_CONTROL = 0x0;
+	while (1 == 1) {
+		*LED_DATA = *SWITCHES_DATA;
+	}
+}
