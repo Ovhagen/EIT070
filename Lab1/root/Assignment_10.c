ed7305d8a0f3055019f03d7af17c9807e5ae734e
Pontus Ovhagen <ovhagen@gmail.com>
2017-02-09 09:27:15 +0100

    changed name on file

--- Lab1/root/Assignment_10.c
+++ Lab1/root/Assignment_10.c
@@ -1,7 +1,7 @@
 #include "address_mapping.h"
 unsigned int state;
 int main() {
-	*SWITCHES_CONTROL = 0xFFFF;
+  *SWITCHES_CONTROL = 0xFFFF;
 	*LED_CONTROL = 0x0;
 	while (1 == 1) {
 		*LED_DATA = *SWITCHES_DATA;
